afebad8528b154de6da47f8067bea0809d9f2518
Mariana Matias Siqueira <mariana.siqueira28900@gmail.com>
2024-04-10 14:33:23 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,57 @@
+let nuns = document.querySelector('.numArray');
+let btnAdicionar = document.getElementById('btn-adicionar');
+let btnFinalizar = document.getElementById('btn-finalizar');
+let areaAnalise = document.getElementById('container-resultado');
+let nunsLista = [];
+
+function inLista(n, l) {
+    if (l.indexOf(n) == -1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+btnAdicionar.onclick = () => {
+    if ((nuns.value >= 1 && nuns.value <= 100) && inLista(nuns.value, nunsLista)) {
+        nunsLista.push(nuns.value);
+
+        document.querySelector('.container-adicionados').innerHTML = nunsLista.join(", ");
+        document.querySelector('.container-adicionados').style.display = "block";
+        areaAnalise.innerHTML = '';
+    }
+    else {
+        window.alert("Valor inválido ou já inserido!");
+    }
+
+    nuns.value = '';
+    nuns.focus();
+}
+
+btnFinalizar.onclick = () => {
+    if (nunsLista.length == 0) {
+        alert("Insira pelo menos um valor na lista.")
+    } else {
+
+        if (nunsLista.length == 1) {
+            areaAnalise.innerHTML = `<p>Ao todo, temos ${nunsLista.length} número cadastrado.</p><br>`;
+        } else {
+            areaAnalise.innerHTML = `<p>Ao todo, temos ${nunsLista.length} números cadastrados.</p><br>`;
+        }
+
+        let menor = nunsLista.reduce((prevValor, valorAtual) => { return Math.min(prevValor, valorAtual) }
+        );
+
+        let maior = nunsLista.reduce((prevValor, valorAtual) => { return Math.max(prevValor, valorAtual) });
+
+        let soma = nunsLista.map(Number).reduce((prevValor, valorAtual) => prevValor + valorAtual);
+
+        let media = soma / nunsLista.length;
+
+        areaAnalise.innerHTML += `
+        <p>O menor valor informado foi ${menor}.</p><br>
+        <p>O maior valor informado foi ${maior}.</p><br>
+        <p>A soma de todos os valores é igual a ${soma}.</p><br>
+        <p>E a média equivale a ${media}.</p>`;
+    }
+}
